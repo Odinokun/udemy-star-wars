50b84053ff5d8ee645e0a3c1e2efe838d6c936e7
odinokun <odinokun@gmail.com>
2019-02-05 14:54:17 +0200

    did clone itemList

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,37 +1,23 @@
 import React, {Component} from 'react';
-
+import './app.css';
 import Header from '../header';
 import RandomPlanet from '../random-planet';
+import PeoplePage from "../people-page/people-page";
 import ItemList from '../item-list';
-import PersonDetails from '../person-details';
-
-import './app.css';
 
 export default class App extends Component {
-
-  state = {
-    selectedPerson: 4
-  };
-
-  onPersonSelected = (id) => {
-    this.setState({
-      selectedPerson: id
-    });
-  };
-
   render() {
     return (
       <div className="app">
         <Header/>
         <RandomPlanet/>
+        <PeoplePage/>
 
         <div className="row mb2">
           <div className="col-md-6">
             <ItemList onItemSelected={this.onPersonSelected}/>
           </div>
-          <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson}/>
-          </div>
+          <div className="col-md-6"></div>
         </div>
       </div>
     );
